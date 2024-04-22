9ed3565de73f13bc61775eceefab8b79b5677d75
fernandezabel <163901256+fernandezabel@users.noreply.github.com>
2024-04-22 20:34:55 -0300

    Add files via upload

---
+++ tp N°3.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int s;
+	float n1,n2,n3;
+
+	cout<<"ingrese numero 1"<<endl;
+	cin>>n1;
+	cout<<"ingrese numero 2"<<endl;
+	cin>>n2;
+	cout<<"ingrese el numero 3"<<endl;
+	cin>>n3;
+	
+	cout<<"el promedio es:  "<<(n1+n2+n3)/3<<endl;
+	
+	
+	
+	return 0;	
+}
